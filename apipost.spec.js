4a40d8a6e537463d2e036348ba097a9861d4759a
Kiruthiga <kiruthiga@qaoncloud.com>
2025-09-23 17:36:38 +0530

    Add files via upload

--- apipost.spec.js
+++ apipost.spec.js
@@ -1,27 +1,35 @@
-// tests/apipost.spec.js
+
 import { test, expect } from '@playwright/test';
 
-test('API GET - Reqres', async ({ request }) => {
-  const response = await request.get('https://reqres.in/api/users?page=2');
+let userId; // shared across tests in this describe block
 
-  expect(response.status()).toBe(200);
+test.describe('JsonPlaceholder API tests', () => {
 
-  const body = await response.json();
-  expect(body.data.some(u => u.first_name === 'Michael')).toBeTruthy();
-
-  console.log('GET response:', body);
-});
-
-test('API POST - Reqres', async ({ request }) => {
-  const response = await request.post('https://reqres.in/api/users', {
-    data: {
-      name: 'kirthi',
-      job: 'leader',
-    },
+test('POST JsonPlaceholder API', async ({ request }) => {
+  const response = await request.post('https://jsonplaceholder.typicode.com/posts', {
+    json: { title: 'foo', body: 'bar', userId: 1 }
   });
 
-  console.log('POST status:', response.status());
-  console.log('POST body:', await response.json());
+  expect(response.status()).toBe(201);
 
-  expect(response.status()).toBe(401);
+  const res = await response.json();
+  console.log('POST response:', res);
+
+  userId = 1; // override to a real existing post
+});
+
+test('PUT JsonPlaceholder API (Update)', async ({ request }) => {
+  const response = await request.put(`https://jsonplaceholder.typicode.com/posts/${userId}`, {
+    json: { title: 'foo', body: 'kir', userId: 1 }
+  });
+
+  console.log('Status:', response.status());
+  console.log('PUT response JSON:', await response.json());
+
+  expect(response.status()).toBe(200);
+});
+test('JsonPlaceholder API delete', async ({ request }) => {
+  const response = await request.delete('https://jsonplaceholder.typicode.com/posts/${userId}')
+  expect(response.status()).toBe(200);
+})
 });
