87b627d9113021c2489df8f4ba2ff715fcfdac74
Kiruthigaqaoncloud <kiruthiga@qaoncloud.com>
2025-10-31 18:13:39 +0530

    Update trace.spec.js

--- trace.spec.js
+++ trace.spec.js
@@ -3,15 +3,12 @@ import { test, expect } from '@playwright/test';
 test('Trace Viewer Example', async ({ page, context }) => {
   // Start tracing (with screenshots & snapshots)
   await context.tracing.start({ screenshots: true, snapshots: true });
-
-  // Your test steps
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.fill('input[name="username"]', 'Admin');
   await page.fill('input[name="password"]', 'admin123');
   await page.click('button[type="submit"]');
-
   await expect(page).toHaveURL(/dashboard/);
-
-  // Stop tracing and save to file
+  // Stop tracing
   await context.tracing.stop({ path: 'trace.zip' });
 });
+
