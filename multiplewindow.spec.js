1460f7dc59036f037d7561de7c9132da22d26f3f
Kiruthigaqaoncloud <kiruthiga@qaoncloud.com>
2025-10-31 18:12:30 +0530

    Update multiplewindow.spec.js

--- multiplewindow.spec.js
+++ multiplewindow.spec.js
@@ -6,21 +6,20 @@ test('Handle OrangeHRM new tab', async ({ browser }) => {
 
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  // Listen for new tab when clicking footer link
+  // new tab when clicking footer link
   const [newPage] = await Promise.all([
     context.waitForEvent('page'),
     page.click('a[href="http://www.orangehrm.com"]'),
   ]);
-
-  // Use domcontentloaded instead of default (safer)
   await newPage.waitForLoadState('domcontentloaded');
 
-  // Print the new page title
+  //new page title
   console.log('New Tab Title:', await newPage.title());
 
-  // Assert the new tab URL contains orangehrm
+  //  new tab URL contains orangehrm
   await expect(newPage).toHaveURL(/orangehrm/);
 
   await newPage.close();
   await context.close();
 });
+
