01322b2049479dd37c836ae58d8c5142f252f484
Kiruthigaqaoncloud <kiruthiga@qaoncloud.com>
2025-10-31 18:11:11 +0530

    Format login.spec.js for consistency

--- login.spec.js
+++ login.spec.js
@@ -1,4 +1,4 @@
-// tests/login.spec.js
+
 import { test } from '@playwright/test';
 import { LoginPage } from '../Pages/LoginPage.js';
 import { DashboardPage } from '../Pages/DashboardPage.js';
@@ -6,8 +6,8 @@ import { DashboardPage } from '../Pages/DashboardPage.js';
 test('Login to OrangeHRM successfully', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const dashboardPage = new DashboardPage(page);
-
   await loginPage.goto();
-  await loginPage.login('Admin', 'admin123'); // demo credentials
+  await loginPage.login('Admin', 'admin123');
   await dashboardPage.assertDashboardVisible();
 });
+
