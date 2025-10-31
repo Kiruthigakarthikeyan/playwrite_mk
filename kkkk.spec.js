4b262ee3c23c9aeb7475584a3b314ec650c35f65
Kiruthigaqaoncloud <kiruthiga@qaoncloud.com>
2025-10-31 18:08:12 +0530

    Fix import and clean up test case syntax

--- kkkk.spec.js
+++ kkkk.spec.js
@@ -1,9 +1,8 @@
 import { test, expect } from '@playwright/test';
-
-// ✅ Example 1: Normal passing test
 test('mytest', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/')
   await page.getByPlaceholder("Username").fill("admin")
   await page.getByPlaceholder("Password").fill("admin123")
   await page.locator("button[type='submit']").click
 });
+
